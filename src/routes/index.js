a4e520ef23ef4a8b4f8f12c982a0dfc19a8dc8f6
sea835 <148954003+sea835@users.noreply.github.com>
2026-05-04 14:06:16 +0700

    implement get product api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const authRoute = require("./auth.route");
+const productRoute = require("./product.route");
 
 const router = Router();
 
 // Mount các route module
 router.use("/auth", authRoute);
+router.use("/products", productRoute);
 
 module.exports = router;
+
